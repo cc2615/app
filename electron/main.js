775f5a84c6689a1cca6324f62f3f209ee0c85c35
caden c <cadenc.woss@gmail.com>
2025-06-04 12:27:23 -0400

    migrate to next.js with tailwind and electron

--- electron/main.js
+++ electron/main.js
@@ -1,19 +1,21 @@
-const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
     },
   });
 
-  win.loadURL('http://localhost:5173');
+  win.loadURL('http://localhost:3000');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
